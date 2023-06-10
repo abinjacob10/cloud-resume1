48c2246ff27b4eea7dde049d154b46322aa9e4f5
abinjacob10 <abinjacob10@gmail.com>
2023-06-10 18:11:36 +0100

    pushing cloud-resume1 to github

---
+++ put_function/app.py
@@ -0,0 +1,61 @@
+import json
+import boto3
+
+
+# import requests
+
+
+def put_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+    # try:
+    #     ip = requests.get("http://checkip.amazonaws.com/")
+    # except requests.RequestException as e:
+    #     # Send some context about this error to Lambda Logs
+    #     print(e)
+
+    #     raise e
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('table1')
+    update = table.update_item(
+
+        Key = {
+                'counts': 'hits' 
+        },
+        UpdateExpression = 'ADD hits :inc',
+        ExpressionAttributeValues = {
+            ':inc': 1
+         }
+
+    )
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': '*'
+        },
+        'body': json.dumps({ "count" : "1" })
+    
+    }
