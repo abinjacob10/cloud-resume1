609cf5d8cc5957b9ec40626cfe5e03ec4c578406
abinjacob10 <abinjacob10@gmail.com>
2023-06-23 10:50:49 +0100

    corrected api

--- test1/api.js
+++ test1/api.js
@@ -1,5 +1,5 @@
 fetch('https://jc5qyxzdo8.execute-api.eu-west-1.amazonaws.com/Prod/put')
-    .then(fetch('https://jc5qyxzdo8.execute-api.eu-west-1.amazonaws.com/Prod/get'))
+    .then(() => fetch('https://jc5qyxzdo8.execute-api.eu-west-1.amazonaws.com/Prod/get'))
     .then(response => response.json())
     .then(data => { document.getElementById('number').innerText = data.count
     })
